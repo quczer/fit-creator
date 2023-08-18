35b2e4aaf55b3c7f3bab4b31a00966d1a5075379
quczer <kucharczi@gmail.com>
2023-08-18 22:01:19 +0000

    fixes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python src/fit_creator/scripts.py download-all-workout-pages --verbose
+python src/fit_creator/main.py download-all-workout-pages --verbose
 python \
     src/fit_creator/main.py export-zwift-workouts-to-wkt \
     --html_path data/html/zwift/workout_plans \
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     license="MIT",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["garmin-fit-sdk", "click", "beautifulsoup4>=4.12"],
+    install_requires=["garmin-fit-sdk", "click", "beautifulsoup4>=4.12", "requests>=2.31.0"],
     extras_require={"dev": ["black", "isort", "ipykernel", "pandas>=2.0"]},
     python_requires=">=3.10",
 )
